6caa25f68f14014f7175125b348469267446745c
alexkharl <sasha_xarlamov@mail.ru>
2024-04-20 23:18:23 +0300

    Ref:Использ-ие niv для nixpkgs

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
+{ sources ? import ./nix/sources.nix }:
 let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/fd16bb6d3bcca96039b11aa52038fafeb6e4f4be.tar.gz"; # unstable channel
-  pkgs = import nixpkgs { config = {}; overlays = []; };
+  pkgs = import sources.nixpkgs { config = {}; overlays = []; };
 in
 {
   build = pkgs.callPackage ./build.nix { };
