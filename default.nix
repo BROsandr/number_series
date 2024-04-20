b6b2a2bb8c3bca562135966af026077de9ece3f6
alexkharl <sasha_xarlamov@mail.ru>
2024-04-20 21:19:17 +0300

    Ref(default.nix):Замена <npkgs> на коммит

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 let
-  pkgs = import <nixpkgs> { config = {}; overlays = []; };
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/fd16bb6d3bcca96039b11aa52038fafeb6e4f4be.tar.gz"; # unstable channel
+  pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 {
   build = pkgs.callPackage ./build.nix { };
