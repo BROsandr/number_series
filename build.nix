9850605225f869c52033c6220d86480dee314743
alexkharl <sasha_xarlamov@mail.ru>
2024-04-20 21:59:20 +0300

    Ref:Испр-ие genericBuilder's фазы

--- build.nix
+++ build.nix
@@ -21,24 +21,4 @@ stdenv.mkDerivation {
     meson
     ninja
   ];
-
-  configurePhase = ''
-    runHook preConfigure
-    mkdir -p build
-    meson setup build
-    runHook postConfigure
-  '';
-
-  buildPhase = ''
-    runHook preBuild
-    meson compile -C build
-    runHook postBuild
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/bin
-    cp ./build/number_series $out/bin
-    runHook postInstall
-  '';
 }
